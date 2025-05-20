4c581c2c5e3b82c8916615e7d37699aa9af21d7f
mipei02091218 <emyutankiiroina31@gmail.com>
2025-05-20 10:49:33 +0900

    noticeモデル単体テストコードの実装

--- spec/models/notice_spec.rb
+++ spec/models/notice_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Notice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:teacher) }
+
+  it 'contentが空だと無効' do
+    notice = Notice.new(user: user, content: '')
+    expect(notice).not_to be_valid
+    expect(notice.errors.full_messages).to include("Content can't be blank")
+  end
 end
